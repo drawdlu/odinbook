959c2c42997e755061c9ee8d429aa4944b0cdf20
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-09-13 17:24:36 +0800

    test: User parameters and validations

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
+  user_name = Faker::Internet.unique.username
   factory :user do
-    
+    username { user_name }
+    email { Faker::Internet.email(name: user_name, domain: "mail.com") }
+    password { 123456 }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "User creation" do
+    context "when complete valid params" do
+      it "will be valid" do
+        new_user = build(:user)
+        expect(new_user).to be_valid
+      end
+    end
+
+    context "when missing username" do
+      it "will be invalid" do
+        new_user = build(:user, username: nil)
+        expect(new_user).to_not be_valid
+      end
+    end
+
+    context "when username has a duplicate" do
+      it "will raise an error when username has a duplicate" do
+        new_user = create(:user)
+          expect do
+              create(:user, username: new_user.username)
+          end.to raise_error(ActiveRecord::RecordInvalid)
+        end
+      end
+    end
 end
