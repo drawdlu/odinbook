70d592f0dd0bef68041b5d03dc2eb852694de7ca
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-09-23 18:10:15 +0800

    feat: Update follow and cancel links on user index

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,15 +2,25 @@ class FollowsController < ApplicationController
   before_action :authenticate_user!
 
   def create
+    @user = User.find(follow_params[:following_id])
     @follow = current_user.followed_users.build(follow_params)
 
     @follow.save
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   def destroy
     @follow = Follow.find(params[:id])
+    @user = User.find(@follow.following_id)
 
     @follow.destroy
+
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   private
