72c94de522bfb34d8825d0c6c740128a08b09e1e
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-09-29 18:19:50 +0800

    feat: Update like count with websocket

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,6 +11,7 @@ class LikesController < ApplicationController
 
     respond_to do |format|
       format.turbo_stream
+      format.html { redirect_to root_path }
     end
   end
 
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,4 +3,16 @@ class Like < ApplicationRecord
   belongs_to :user
 
   validates :post_id, uniqueness: { scope: :user_id }
+
+  after_create :broadcast_like
+  after_destroy :broadcast_like
+
+  def broadcast_like
+    broadcast_update_to(
+      "like",
+      partial: "likes/like_count",
+      target: "like_count_#{post.id}",
+      locals: { post: self.post }
+    )
+  end
 end
