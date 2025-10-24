eb3f77037e4ea28f2c5643cd241bd72dea4fa51a
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-10-24 17:50:27 +0800

    feat: Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,37 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'factory_bot_rails'
+
+include FactoryBot::Syntax::Methods
+
+# Users
+# all users will have a password of 1234Aa*&
+users = []
+10.times do
+  users.push(create(:user))
+end
+
+# Follows
+15.times do
+  following = users.sample
+  follower = (users - [ following ]).sample
+
+  Follow.find_or_create_by(following: following, follower: follower, status: 1)
+end
+
+# Posts
+posts = []
+10.times do
+  posts.push(create(:post, user: users.sample))
+end
+
+# Comments
+10.times do
+  create(:comment, user: users.sample, post: posts.sample)
+end
+
+# Likes
+10.times do
+  Like.find_or_create_by(post: posts.sample, user: users.sample)
+end
