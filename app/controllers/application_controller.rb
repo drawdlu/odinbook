d4e4dab2d3481774382f1e4187d18876a84f9f45
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-10-28 19:39:37 +0800

    fix: Notification to login after sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,4 +19,8 @@ class ApplicationController < ActionController::Base
   def set_current_user
     Current.user = current_user
   end
+
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
 end
