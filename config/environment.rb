c77de0e417aac020574cd07db6de4440b02f90bc
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-11-01 16:12:19 +0800

    build: Load environment variable for api key

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
   user_name: "apikey", # This is the string literal 'apikey', NOT the ID of your API key
-  password: Rails.application.credentials.dig(:sendgrid_api_key), # This is the secret sendgrid API key which was issued during API key creation
+  password: ENV["SENDGRID_API_KEY"], # This is the secret sendgrid API key which was issued during API key creation
   domain: "https://mysite-hbrl.onrender.com",
   address: "smtp.sendgrid.net",
   port: 587,
