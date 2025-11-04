ee2285222e57af7632f0a3da26ca1dbab9908050
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-11-04 18:49:51 +0800

    feat: Show latest post first in user profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @profile = @user.profile
-    @posts = @user.posts.includes(:likes, :comments)
+    @posts = @user.posts.includes(:likes, :comments).reverse
   end
 end
